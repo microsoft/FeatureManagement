ad9bba902c8e27a5e92952625df104811cd0399a
Ross Grambo <rossgrambo@microsoft.com>
2024-10-24 15:50:44 -0700

    Adds .net Validation Tests

---
+++ libraryValidations/Dotnet/SharedTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.FeatureManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet
+{
+    internal class SharedTest
+    {
+        public string FeatureFlagName { get; set; }
+        public InputsSection Inputs { get; set; }
+        public IsEnabledSection IsEnabled { get; set; }
+        public VariantSection Variant { get; set; }
+        public string Description { get; set; }
+
+
+        internal class InputsSection
+        {
+            public string user { get; set; }
+            public string[] groups { get; set; }
+        }
+
+        internal class IsEnabledSection
+        {
+            public string Result { get; set; }
+            public string Exception { get; set; }
+        }
+
+        internal class VariantSection
+        {
+            public string Result { get; set; }
+            public string Exception { get; set; }
+        }
+    }
+}
---
+++ libraryValidations/Dotnet/test_validations.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+using Dotnet;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.FeatureFilters;
+using System.Text.Json;
+
+[TestClass]
+public class Tests
+{
+    private const string FilePath = "../../../../../Samples/";
+    private const string SampleJsonKey = ".sample.json";
+    private const string TestsJsonKey = ".tests.json";
+
+    public required IFeatureManager featureManager;
+
+    [TestMethod]
+    public async Task TestNoFilters()
+    {
+        await RunTests("NoFilters");
+    }
+
+    [TestMethod]
+    public async Task TestTimeWindowFilter()
+    {
+        await RunTests("TimeWindowFilter");
+    }
+
+    [TestMethod]
+    public async Task TestTargetingFilter()
+    {
+        await RunTests("TargetingFilter");
+    }
+
+    [TestMethod]
+    public async Task TestTargetingFilterModified()
+    {
+        await RunTests("TargetingFilter.modified");
+    }
+
+    [TestMethod]
+    public async Task TestRequirementType()
+    {
+        await RunTests("RequirementType");
+    }
+
+    [TestMethod]
+    public async Task TestBasicVariant()
+    {
+        await RunTests("BasicVariant");
+    }
+
+    private async Task RunTests(string testKey)
+    {
+        // Use Sample JSON as Configuration
+        string file = Directory.GetFiles(FilePath, testKey + SampleJsonKey).First();
+
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+
+        builder.AddJsonFile(Path.GetFullPath(file));
+
+        IConfiguration configuration = builder.Build();
+
+        // Setup FeatureManagement
+        IServiceCollection services = new ServiceCollection();
+
+        services.AddSingleton(configuration)
+                .AddFeatureManagement();
+
+        ServiceProvider serviceProvider = services.BuildServiceProvider();
+        featureManager = serviceProvider.GetRequiredService<IFeatureManager>();
+
+        // Get Test Suite JSON
+        var featureFlagTests = JsonSerializer.Deserialize<SharedTest[]>(File.ReadAllText(FilePath + testKey + TestsJsonKey));
+
+        if (featureFlagTests == null) {
+            throw new Exception("Test failed to parse JSON");
+        }
+
+        // Run each test
+        foreach (var featureFlagTest in featureFlagTests)
+        {
+            bool? expectedIsEnabledResult = featureFlagTest.IsEnabled?.Result != null ? Convert.ToBoolean(featureFlagTest.IsEnabled?.Result) : null;
+            string featureFlagId = testKey + "." + featureFlagTest.FeatureFlagName;
+            string failedDescription = $"Test {featureFlagId} failed. Description: {featureFlagTest.Description}";
+
+            if (featureFlagTest.IsEnabled != null)
+            {
+                if (expectedIsEnabledResult != null)
+                {
+                    bool isEnabledResult = await featureManager.IsEnabledAsync(featureFlagTest.FeatureFlagName, new TargetingContext { UserId = featureFlagTest.Inputs.user, Groups = featureFlagTest.Inputs.groups });
+                    Assert.AreEqual(expectedIsEnabledResult, isEnabledResult, failedDescription);
+                }
+                else
+                {
+                    await Assert.ThrowsExceptionAsync<FeatureManagementException>(async () => await featureManager.IsEnabledAsync(featureFlagTest.FeatureFlagName), featureFlagTest.IsEnabled.Exception);
+                }
+            }
+        }
+    }
+}
