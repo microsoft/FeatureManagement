e5142b9f88b93edc9ec75aac42940fe64834b98b
Ross Grambo <rossgrambo@microsoft.com>
2024-10-31 12:02:07 -0700

    Fixes test call in shared script

--- libraryValidations/run_all_tests.ps1
+++ libraryValidations/run_all_tests.ps1
@@ -7,7 +7,7 @@ Set-Location -Path "../Python"
 python -m venv venv
 .\venv\Scripts\activate.ps1
 pip install -r requirements.txt
-pytest test_json_validations.py
+pytest
 deactivate
 
 # Navigate to the Spring directory and run mvn test
