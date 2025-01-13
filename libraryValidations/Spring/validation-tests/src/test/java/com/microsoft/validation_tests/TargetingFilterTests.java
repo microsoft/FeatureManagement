1c8c72bb2e61586353d13f1398796a0be801b2e9
Matthew Metcalf <mrm9084@gmail.com>
2025-01-13 15:23:22 -0800

    Spring Feature Variants Validation (#21)
    * Feature Variants
    
    * Update libraryValidations/Spring/validation-tests/src/test/java/com/microsoft/validation_tests/ValidationTestsApplicationTests.java
    
    Co-authored-by: Ross Grambo <rossgrambo@microsoft.com>
    
    ---------
    
    Co-authored-by: Ross Grambo <rossgrambo@microsoft.com>

--- libraryValidations/Spring/validation-tests/src/test/java/com/microsoft/validation_tests/TargetingFilterTests.java
+++ libraryValidations/Spring/validation-tests/src/test/java/com/microsoft/validation_tests/TargetingFilterTests.java
@@ -2,12 +2,10 @@ package com.microsoft.validation_tests;
 
 import java.io.IOException;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
@@ -16,11 +14,9 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 @SpringBootTest(classes = { SpringBootTest.class, Filters.class })
 @EnableConfigurationProperties
 @ComponentScan(basePackages = { "com.azure.spring.cloud.feature.management" })
-@ActiveProfiles("TargetingFilter")
 class TargetingFilterTests extends ValidationTestsApplicationTests {
 
     @Test
-    @Disabled("Wrong Endian used.")
     void validateTest() throws IOException {
         runTests("TargetingFilter");
     }
